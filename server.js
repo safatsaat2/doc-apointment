ff9f506fe96799bb215a19c1c5abf4c0952c85f6
Sakib Rahman Safat <sakibsafat47@gmail.com>
2023-08-05 17:02:27 +0600

    server created

---
+++ server.js
@@ -0,0 +1,29 @@
+const express = require('express')
+const colors = require("colors")
+const morgan = require("morgan")
+const dotenv = require("dotenv")
+
+dotenv.config()
+
+// Rest Object
+
+const app = express()
+
+// middlewares
+app.use(express.json())
+app.use(morgan('dev'))
+
+
+// routes
+app.get('/', (req, res)=>{
+    res.status(200).send({
+        message: "Server is running",
+    })
+})
+
+const port = process.env.PORT || 8080
+
+// Listen
+app.listen(port, ()=>{
+    console.log(`Server  is running in ${process.env.DEV_MODE} Mode on port ${process.env.PORT}`.bgBlue.white)
+})
